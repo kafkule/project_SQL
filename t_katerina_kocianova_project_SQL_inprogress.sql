c8c228bf54ca18deebdf1c2654874183cb572a2b
Katerina Kocianova <wicana@centrum.cz>
2023-10-14 09:02:13 +0200

    Inserted

---
+++ t_katerina_kocianova_project_SQL_inprogress.sql
@@ -0,0 +1,52 @@
+-- Datové sady, které je možné použít pro získání vhodného datového podkladu
+
+-- Primární tabulky:
+
+SELECT *
+FROM czechia_payroll; -- Informace o mzdách v různých odvětvích za několikaleté období. Datová sada pochází z Portálu otevřených dat ČR.
+
+SELECT *
+FROM czechia_payroll_calculation; -- Číselník kalkulací v tabulce mezd.
+
+SELECT *
+FROM czechia_payroll_industry_branch; -- Číselník odvětví v tabulce mezd.
+
+SELECT *
+FROM czechia_payroll_unit; -- Číselník jednotek hodnot v tabulce mezd.
+
+SELECT *
+FROM czechia_payroll_value_type; -- Číselník typů hodnot v tabulce mezd.
+
+SELECT *
+FROM czechia_price; -- Informace o cenách vybraných potravin za několikaleté období. Datová sada pochází z Portálu otevřených dat ČR.
+
+SELECT *
+FROM czechia_price_category; -- Číselník kategorií potravin, které se vyskytují v našem přehledu.
+
+
+-- Číselníky sdílených informací o ČR:
+
+SELECT *
+FROM czechia_region; -- Číselník krajů České republiky dle normy CZ-NUTS 2.
+
+SELECT *
+FROM czechia_district; -- Číselník okresů České republiky dle normy LAU.
+
+
+-- Dodatečné tabulky:
+
+SELECT *
+FROM countries; -- Všemožné informace o zemích na světě, například hlavní město, měna, národní jídlo nebo průměrná výška populace.
+
+SELECT *
+FROM economies; -- HDP, GINI, daňová zátěž, atd. pro daný stát a rok.
+
+
+/*
+Výzkumné otázky
+1) Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?
+2) Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední srovnatelné období v dostupných datech cen a mezd?
+3) Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční nárůst)?
+4) Existuje rok, ve kterém byl meziroční nárůst cen potravin výrazně vyšší než růst mezd (větší než 10 %)?
+5) Má výška HDP vliv na změny ve mzdách a cenách potravin? Neboli, pokud HDP vzroste výrazněji v jednom roce, projeví se to na cenách potravin či mzdách ve stejném nebo násdujícím roce výraznějším růstem?
+*/
