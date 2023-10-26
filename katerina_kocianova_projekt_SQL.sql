a61cc18072bd2f0d8ac22725d8c265c88d113b19
Katerina Kocianova <wicana@centrum.cz>
2023-10-26 23:22:27 +0200

    Update SQL project

--- katerina_kocianova_projekt_SQL.sql
+++ katerina_kocianova_projekt_SQL.sql
@@ -64,6 +64,25 @@ JOIN czechia_payroll_value_type AS cpvt ON cpay.value_type_code = cpvt.code
 JOIN czechia_price AS cp ON cpay.payroll_year = YEAR(cp.date_from)
 RIGHT JOIN czechia_price_category AS cpcat ON cp.category_code = cpcat.code;
 
+SELECT
+	ib.name AS industry,
+	cpay.value AS payroll_value,
+	cpay.payroll_year AS 'year',
+	ROUND(AVG(cp.value),2) AS food_value,
+	cpcat.name AS food_name
+FROM czechia_payroll AS cpay
+JOIN czechia_payroll_industry_branch AS ib ON cpay.industry_branch_code = ib.code
+JOIN czechia_payroll_calculation AS cpc ON cpay.calculation_code = cpc.code
+JOIN czechia_payroll_unit AS cpu ON cpay.unit_code = cpu.code
+JOIN czechia_payroll_value_type AS cpvt ON cpay.value_type_code = cpvt.code
+JOIN czechia_price AS cp ON cpay.payroll_year = YEAR(cp.date_from)
+RIGHT JOIN czechia_price_category AS cpcat ON cp.category_code = cpcat.code
+WHERE cpay.value_type_code = 5958
+	AND cpay.calculation_code = 200 
+	AND cpay.industry_branch_code IS NOT NULL
+GROUP BY industry, 'year'
+ORDER BY industry, 'year';
+
 -- CREATE OR REPLACE TABLE `t_Katerina_Kocianova_project_SQL_primary_final` AS
 SELECT
 	ib.name AS industry,
@@ -80,9 +99,7 @@ JOIN czechia_price AS cp ON cpay.payroll_year = YEAR(cp.date_from)
 RIGHT JOIN czechia_price_category AS cpcat ON cp.category_code = cpcat.code
 WHERE cpay.value_type_code = 5958
 	AND cpay.calculation_code = 200 
-	AND cpay.industry_branch_code IS NOT NULL
-	AND cpcat.code IN (111301, 114201)
-ORDER BY industry;
+	AND cpay.industry_branch_code IS NOT NULL;
 
 
 -- CREATE OR REPLACE TABLE `t_Katerina_Kocianova_project_SQL_secondary_final` AS
