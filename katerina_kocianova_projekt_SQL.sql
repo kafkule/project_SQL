f910dd9a4ecac18d68dcbf9bae814052fd8088c2
Katerina Kocianova <wicana@centrum.cz>
2023-10-27 13:29:02 +0200

    Update SQL project

--- katerina_kocianova_projekt_SQL.sql
+++ katerina_kocianova_projekt_SQL.sql
@@ -54,21 +54,14 @@ B) Zodpovědět výzkumné otázky:
 */
 
 -- VÝSTUP PROJEKTU - TABULKY
-
-SELECT *
-FROM czechia_payroll AS cpay
-JOIN czechia_payroll_industry_branch AS ib ON cpay.industry_branch_code = ib.code
-JOIN czechia_payroll_calculation AS cpc ON cpay.calculation_code = cpc.code
-JOIN czechia_payroll_unit AS cpu ON cpay.unit_code = cpu.code
-JOIN czechia_payroll_value_type AS cpvt ON cpay.value_type_code = cpvt.code
-JOIN czechia_price AS cp ON cpay.payroll_year = YEAR(cp.date_from)
-RIGHT JOIN czechia_price_category AS cpcat ON cp.category_code = cpcat.code;
-
+	
+-- CREATE OR REPLACE TABLE `t_katerina_kocianova_project_SQL_primary_final` AS
 SELECT
 	ib.name AS industry,
-	cpay.value AS payroll_value,
-	cpay.payroll_year AS 'year',
-	ROUND(AVG(cp.value),2) AS food_value,
+	AVG(cpay.value) AS avg_payroll_value,
+	cpay.payroll_year AS payroll_value_year,
+	AVG(cp.value) AS avg_food_value,
+	YEAR(cp.date_from) AS food_value_year,
 	cpcat.name AS food_name
 FROM czechia_payroll AS cpay
 JOIN czechia_payroll_industry_branch AS ib ON cpay.industry_branch_code = ib.code
@@ -80,29 +73,10 @@ RIGHT JOIN czechia_price_category AS cpcat ON cp.category_code = cpcat.code
 WHERE cpay.value_type_code = 5958
 	AND cpay.calculation_code = 200 
 	AND cpay.industry_branch_code IS NOT NULL
-GROUP BY industry, 'year'
-ORDER BY industry, 'year';
+GROUP BY industry, payroll_value_year, food_value_year, food_name
+ORDER BY industry ASC, payroll_value_year ASC, food_value_year, food_name ASC;
 
--- CREATE OR REPLACE TABLE `t_Katerina_Kocianova_project_SQL_primary_final` AS
-SELECT
-	ib.name AS industry,
-	cpay.value AS payroll_value,
-	cpay.payroll_year AS 'year',
-	cp.value AS food_value,
-	cpcat.name AS food_name
-FROM czechia_payroll AS cpay
-JOIN czechia_payroll_industry_branch AS ib ON cpay.industry_branch_code = ib.code
-JOIN czechia_payroll_calculation AS cpc ON cpay.calculation_code = cpc.code
-JOIN czechia_payroll_unit AS cpu ON cpay.unit_code = cpu.code
-JOIN czechia_payroll_value_type AS cpvt ON cpay.value_type_code = cpvt.code
-JOIN czechia_price AS cp ON cpay.payroll_year = YEAR(cp.date_from)
-RIGHT JOIN czechia_price_category AS cpcat ON cp.category_code = cpcat.code
-WHERE cpay.value_type_code = 5958
-	AND cpay.calculation_code = 200 
-	AND cpay.industry_branch_code IS NOT NULL;
-
-
--- CREATE OR REPLACE TABLE `t_Katerina_Kocianova_project_SQL_secondary_final` AS
+-- CREATE OR REPLACE TABLE `t_katerina_kocianova_project_SQL_secondary_final` AS
 SELECT 
 	c.country,
 	c.capital_city,
