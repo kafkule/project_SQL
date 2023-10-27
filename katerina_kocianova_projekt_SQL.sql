f9977aa9d217c11f90900e8e7182e4afb8e56d52
Katerina Kocianova <wicana@centrum.cz>
2023-10-27 14:52:53 +0200

    Update SQL project

--- katerina_kocianova_projekt_SQL.sql
+++ katerina_kocianova_projekt_SQL.sql
@@ -44,7 +44,7 @@ FROM economies; -- HDP, GINI, daňová zátěž, atd. pro daný stát a rok.
 
 /* 
 Úkoly:
-A) Vytvořit tabulky t_Katerina_Kocianova_project_SQL_primary_final a t_Katerina_Kocianova_project_SQL_secondary_final
+A) Vytvořit tabulky t_katerina_kocianova_project_SQL_primary_final a t_katerina_kocianova_project_SQL_secondary_final
 B) Zodpovědět výzkumné otázky:
 	1) Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?
 	2) Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední srovnatelné období v dostupných datech cen a mezd?
@@ -53,11 +53,14 @@ B) Zodpovědět výzkumné otázky:
 	5) Má výška HDP vliv na změny ve mzdách a cenách potravin? Neboli, pokud HDP vzroste výrazněji v jednom roce, projeví se to na cenách potravin či mzdách ve stejném nebo násdujícím roce výraznějším růstem?
 */
 
+
 -- VÝSTUP PROJEKTU - TABULKY
 	
+
 -- CREATE OR REPLACE TABLE `t_katerina_kocianova_project_SQL_primary_final` AS
 SELECT
 	ib.name AS industry,
+	cpay.industry_branch_code,
 	AVG(cpay.value) AS avg_payroll_value,
 	cpay.payroll_year AS payroll_value_year,
 	AVG(cp.value) AS avg_food_value,
@@ -75,6 +78,7 @@ WHERE cpay.value_type_code = 5958
 	AND cpay.industry_branch_code IS NOT NULL
 GROUP BY industry, payroll_value_year, food_value_year, food_name
 ORDER BY industry ASC, payroll_value_year ASC, food_value_year, food_name ASC;
+
 
 -- CREATE OR REPLACE TABLE `t_katerina_kocianova_project_SQL_secondary_final` AS
 SELECT 
@@ -127,7 +131,37 @@ WHERE cpay.value_type_code = 5958 AND cpay2.value_type_code = 5958
 GROUP BY industry, time_period
 ORDER BY industry, time_period;
 
+SELECT 
+    kkp.industry,	
+	kkp.payroll_value_year AS 'year', 
+	kkp.avg_payroll_value,
+	kkp2.payroll_value_year AS previous_year, 
+	kkp2.avg_payroll_value,
+	ROUND(((kkp.avg_payroll_value - kkp2.avg_payroll_value) / kkp2.avg_payroll_value) * 100, 2) AS payroll_value_growth_percentage
+FROM t_katerina_kocianova_project_SQL_primary_final AS kkp
+JOIN t_katerina_kocianova_project_SQL_primary_final AS kkp2
+ON kkp.payroll_value_year = kkp2.payroll_value_year + 1
+	AND kkp.industry_branch_code = kkp2.industry_branch_code
+;
 
+
+SELECT 
+    ib.name AS industry,	
+	cpay.payroll_year AS time_period, 
+	cpay.value AS payroll_value,
+	cpay2.payroll_year AS previous_time_period,
+	cpay2.value AS previous_time_period_value,
+	ROUND(((cpay.value - cpay2.value) / cpay2.value) * 100, 2) AS value_growth_percentage
+FROM czechia_payroll AS cpay
+JOIN czechia_payroll AS cpay2 ON cpay.payroll_year = cpay2.payroll_year + 1
+	AND cpay.industry_branch_code = cpay2.industry_branch_code
+JOIN czechia_payroll_industry_branch AS ib ON cpay.industry_branch_code = ib.code
+WHERE cpay.value_type_code = 5958 AND cpay2.value_type_code = 5958
+	AND cpay.calculation_code = 200 AND cpay2.calculation_code = 200 
+	AND cpay.industry_branch_code IS NOT NULL -- průměrná hrubá mzda za plný úvazek v oboru
+	AND cpay.payroll_year BETWEEN '2006' AND '2018'
+GROUP BY industry, time_period
+ORDER BY industry, time_period;
 
 -- 2) Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední srovnatelné období v dostupných datech cen a mezd?
 
